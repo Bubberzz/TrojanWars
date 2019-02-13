ed189adb649c2813468d58211a0bdaa5ee525db0
Stan Bubbers <stan.bubbers@live.co.uk>
2019-02-13 14:18:28 +0000

    created Tools class for font colour. created AtackResult method in Hero class. Modified WriteLine commands

--- TrojanWars/Hero.cs
+++ TrojanWars/Hero.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Threading;
 using TrojanWars.Enum;
 using TrojanWars.Equipment;
 namespace TrojanWars
 {
     class Hero
     {
-        private const int TROJAN_STARTING_HEALTH = 100;
-        private const int GREEK_STARTING_HEALTH = 100;
+        private const int TROJAN_STARTING_HEALTH = 10;
+        private const int GREEK_STARTING_HEALTH = 10;
 
         private readonly Allegiance ALLEGIANCE;
 
@@ -51,14 +52,25 @@ namespace TrojanWars
             int damage = weapon.Damage / enemyHero.armour.ArmourPoints;
             enemyHero.health = enemyHero.health - damage;
 
+            AttackResult(enemyHero, damage);
+
+            Thread.Sleep(100);
+            
+        }
+
+        private void AttackResult(Hero enemyHero, int damage)
+        {
             if (enemyHero.health <= 0)
             {
                 enemyHero.isAlive = false;
-                Console.WriteLine($"{enemyHero.name} is dead. {name} is victorious!");
-                Console.ReadLine();
+                Tools.ColourOfWriteLine($"{enemyHero.name} is dead.", ConsoleColor.Red);
+                Tools.ColourOfWriteLine($"{name} is victorious!", ConsoleColor.Green);
             }
-
-            
+            else
+            {
+                Console.WriteLine(
+                    $"{name} attacked {enemyHero.name}. {damage} damage was inflicted to {enemyHero.name}, remaining health of {enemyHero.name} is {enemyHero.health}.");
+            }
         }
     }
 }
---
+++ TrojanWars/Tools.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrojanWars
+{
+    static class Tools
+    {
+        public static void ColourOfWriteLine(string text, ConsoleColor colour)
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
